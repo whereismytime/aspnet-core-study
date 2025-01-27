f02a5ba62a8736985ee88e62cac627e45810c93a
whereismytime <a.v.dyma@gmail.com>
2025-01-27 17:10:00 +0000

    Add 01_AnimalHierarchy project

---
+++ 01_AnimalHierarchy/AnimalHierarchy/AnimalHierarchy/Services/ConsoleOutput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AnimalHierarchy
+{
+    /// <summary>
+    /// Console-based implementation of the IOutput interface.
+    /// Outputs messages to the console.
+    /// </summary>
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message); // Display message in the console
+        }
+    }
+}
